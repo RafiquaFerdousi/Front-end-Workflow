9cf41b333e9efd8f60a091d8fe847c6b4d0cc714
Rafiqua Ferdousi <44333720+RafiquaFerdousi@users.noreply.github.com>
2023-06-28 14:06:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,56 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
 }
 // Call styleAndCelebrate
 styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', 'champions');
+
+//OOP
+
+// Task 1: Code a Person class
+class Person {
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+
+    sleep() {
+        this.energy += 10;
+    }
+  
+    doSomethingFun() {
+        this.energy -= 10;
+    }
+    
+}
+
+const person = new Person();
+person.sleep();
+person.doSomethingFun();
+
+// Task 2: Code a Worker class
+class Worker extends Person {
+    constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+
+    goToWork() {
+        this.xp += 10;
+    }
+}
+const worker = new Worker();
+worker.goToWork();
+
+// Task 3: Code an intern object, run methods
+function intern() {
+  const internObject = new Worker("Bob", 21, 110, 0, 10);
+  internObject.goToWork();
+  return internObject;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+    const managerObject = new Worker("Alice", 30, 120, 100, 30);
+  internObject.doSomethingFun();
+  return managerObject;
+}
