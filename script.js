a83909b4ebd6686fdf8a6cb4fb4cc893c8ef7ef3
Rafiqua Ferdousi <44333720+RafiquaFerdousi@users.noreply.github.com>
2023-06-29 13:16:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -141,3 +141,21 @@ function animalCan() {
     }
 }
 animalCan();
+
+
+//Testing
+
+// Task 1: Code the timesTwo function declaration
+function timesTwo(n) {
+    return n*2;
+};
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
+
+
+// Task 1: Code the timesTwo function declaration
+function timesTwo(n) {
+    return n*2;
+};
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
